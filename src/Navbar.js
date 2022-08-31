3dec167abb50a6a716bb5240a912df1dd4d9c685
Priyanshu <priyanshupuri0@gmail.com>
2022-08-31 22:05:21 +0530

    refresh button issue is resolved

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 export default function Navbar() {
     const refreshSession = () => {
-        window.location.reload();
+        window.location.href = '/';
     }
     return (
         <div>
