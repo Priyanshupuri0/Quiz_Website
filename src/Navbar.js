89ce5744a1e157c7cf1d97466f6def5541d982ec
Priyanshu <priyanshupuri0@gmail.com>
2022-09-05 20:28:52 +0530

    reload problem is resolved

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 export default function Navbar() {
     const refreshSession = () => {
-        window.location.href = '/';
+        window.location.href = '/QuizPanel';
     }
     return (
         <div>
